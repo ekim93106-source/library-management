761a526081a2031a842626bda95e627b4f8f660e
kimani <ekim93106@gmail.com>
2026-03-02 20:59:25 +0300

    update task class implementation

--- task.py
+++ task.py
@@ -1,10 +1,13 @@
 class Task:
-    def __init__(self, title):
+    def __init__(self, title, book):
         self.title = title
         self.completed = False
+        self.book = book
+        book.attach_task(self)
 
     def mark_complete(self):
         self.completed = True
+        self.book.mark_completed()
 
     def __str__(self):
         status = "Completed" if self.completed else "Not Completed"
